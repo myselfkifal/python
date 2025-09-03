860e9525f507f2fd5e76507409931be3b6dd023b
myselfkifal <myselfkifal@gmail.com>
2025-09-03 13:04:21 +0500

    Day 3

---
+++ Day 3/task1.py
@@ -0,0 +1,7 @@
+# Python basic (lists, tuples, and dictionaries)
+# Task : Create a list of favorite subjects and print them one by one.
+
+subject= ["CSS", "HTML", "JavaScript", "Python", "Django"] # list of favorite subjects
+print("My fav subjects", subject) # printing the list
+for sub in subject: # iterating through the list
+    print(sub) # printing each subject one by one
